916297b4a0487b14c84818537946c55000c129f1
alejoguerrero5 <alejoguerrero5@gmail.com>
2023-03-12 13:12:16 -0500

    Validación presupuesto valido

--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,26 +1,40 @@
-import React from 'react'
+import { useState } from "react";
+import Mensaje from "./Mensaje";
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-  return (
-    <div className='contenedor-presupuesto contenedor sombra'>
-      <form className='formulario'>
-        <div className='campo'>
-            <label>Definir Presupuesto</label>
+const NuevoPresupuesto = ({ presupuesto, setPresupuesto }) => {
 
-            <input 
-                className='nuevo-presupuesto'
-                type='text'
-                placeholder='Añade tu presupuesto'
-                value={presupuesto}
-                onChange={(e)=> setPresupuesto(e.target.value)}
-            />
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+
+        if (!Number(presupuesto) || Number(presupuesto)<0) {
+            setMensaje('No es un presupuesto valido')
+        } else { setMensaje('Es presupuesto valido') }
+    }
+
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
+                <div className='campo'>
+                    <label>Definir Presupuesto</label>
+
+                    <input
+                        className='nuevo-presupuesto'
+                        type='text'
+                        placeholder='Añade tu presupuesto'
+                        value={presupuesto}
+                        onChange={(e) => setPresupuesto(e.target.value)}
+                    />
+                </div>
+
+                <input type='submit' value='Añadir' />
+
+                {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
+
+            </form>
         </div>
-
-        <input type='submit' value='Añadir'/>
-
-      </form>
-    </div>
-  )
+    )
 }
 
 export default NuevoPresupuesto
