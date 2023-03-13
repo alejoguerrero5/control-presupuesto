72922c57c2b7c6714a7f83a60102f6eb6eaa90f3
alejoguerrero5 <alejoguerrero5@gmail.com>
2023-03-12 23:09:03 -0500

    modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import Modal from './components/Modal';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
+
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true)
+  }
 
   return (
     <div>
@@ -21,9 +28,15 @@ function App() {
           <img
             src={IconoNuevoGasto}
             alt='icono nuevo gasto'
+            onClick={handleNuevoGasto}
           />
         </div>
       ) }
+
+      {modal && 
+      <Modal 
+        setModal={setModal}
+      />}
 
 
     </div>
