2eb08c083d4de6ba996ced6b27772e069cdce4d3
alejoguerrero5 <alejoguerrero5@gmail.com>
2023-03-13 14:53:30 -0500

    calculo gasto y disponible

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
 
-const ControlPresupuesto = ({presupuesto}) => {
+const ControlPresupuesto = ({presupuesto, gastos}) => {
+
+    const [disponible, setDisponible] = useState(0);
+    const [gastado, setGastado] = useState();
+
+
+    useEffect(() => {
+        const totalGastado = gastos.reduce((total, gasto)=> gasto.cantidad + total,0) //acumula un valor
+
+        const totalDisponible = presupuesto-totalGastado
+
+        setGastado(totalGastado)
+        setDisponible(totalDisponible)
+
+    }, [gastos]);
 
     const formatearCantidad = (cantidad) => {
-        return cantidad.toLocaleString('es-CO',{
+        return cantidad.toLocaleString('en-US',{
             style: 'currency',
-            currency: 'COP'
-        })
+            currency: 'USD'
+        }) 
     }
 
   return (
@@ -20,10 +34,10 @@ const ControlPresupuesto = ({presupuesto}) => {
                 <span>Presupuesto: </span>{formatearCantidad(presupuesto)}
             </p>
             <p>
-                <span>Gastado: </span>{formatearCantidad(0)}
+                <span>Gastado: </span>{formatearCantidad(gastado)}
             </p>
             <p>
-                <span>Disponible: </span>{formatearCantidad(0)}
+                <span>Disponible: </span>{formatearCantidad(disponible)}
             </p>
         </div>
       
