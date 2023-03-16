3c1875a6c191ca2bee002d56a6cb83d20af01caa
alejoguerrero5 <alejoguerrero5@gmail.com>
2023-03-16 08:25:25 -0500

    filtros 2

--- src/components/ListadoGastos.jsx
+++ src/components/ListadoGastos.jsx
@@ -11,35 +11,35 @@ const ListadoGastos = ({
 
   return (
     <div className='listado-gastos contenedor'>
-      
+
 
       {/* Si hay un filtro itera sobre gastos filtrados, sino itera sobre los gastos totales */}
       {
         filtro ? (
           <>
-          <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos en esta categoria'}</h2>
+            <h2>{gastosFiltrados.length ? 'Gastos' : 'No hay gastos en esta categoría'}</h2>
 
-          {gastosFiltrados.map(gasto => (
-            <Gasto
-              key={gasto.id}
-              gasto={gasto}
-              setGastoEditar={setGastoEditar}
-              eliminarGasto={eliminarGasto}
-            />
-          ))}
+            {gastosFiltrados.map(gasto => (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            ))}
           </>
         ) : (
           <>
-          <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
+            <h2>{gastos.length ? 'Gastos' : 'No hay gastos aún'}</h2>
 
-          {gastos.map(gasto => (
-            <Gasto
-              key={gasto.id}
-              gasto={gasto}
-              setGastoEditar={setGastoEditar}
-              eliminarGasto={eliminarGasto}
-            />
-          ))}
+            {gastos.map(gasto => (
+              <Gasto
+                key={gasto.id}
+                gasto={gasto}
+                setGastoEditar={setGastoEditar}
+                eliminarGasto={eliminarGasto}
+              />
+            ))}
           </>
         )
       }
